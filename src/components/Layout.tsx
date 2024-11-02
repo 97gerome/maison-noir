06b7f0ba1c583f703450a2104c4c2da206f40897
Gerome Mandapat <geromemandapat@Geromes-MacBook-Pro-2.local>
2024-11-02 19:50:20 +0800

    Created the Layout component.

---
+++ src/components/Layout.tsx
@@ -0,0 +1,27 @@
+import { FC, useState } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import { LayoutContext } from "../hooks/useLayout";
+
+const Layout: FC = () => {
+  const [isHeaderVisible, setisHeaderVisible] = useState<boolean>(false);
+
+  const toggleIsHeaderVisible = (): void => {
+    setisHeaderVisible((current) => !current);
+  };
+
+  return (
+    <div className="layout">
+      {isHeaderVisible && <Header />}
+      <main>
+        <Outlet
+          context={
+            { isHeaderVisible, toggleIsHeaderVisible } satisfies LayoutContext
+          }
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
