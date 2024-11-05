6d075f48745310384584efc8984b698c3f90dc78
Gerome Mandapat <geromemandapat@Geromes-MacBook-Pro-2.local>
2024-11-05 18:16:20 +0800

    Refactored to map through the pages array and added hover handlers to onHoverStart and onHoverEnd events.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,14 @@
 import { AnimationProps, motion } from "framer-motion";
 import { FC } from "react";
 import { useNavigate } from "react-router-dom";
+import useCursor from "../hooks/useCursor";
+
+const pages: Array<{ title: string; url: string }> = [
+  { title: "Home", url: "/" },
+  { title: "About", url: "/about" },
+  { title: "Projects", url: "/projects" },
+  { title: "Contact", url: "/contact" },
+];
 
 const navLinkInitial: AnimationProps["initial"] = {
   opacity: 0,
@@ -8,47 +16,26 @@ const navLinkInitial: AnimationProps["initial"] = {
 };
 
 const Nav: FC = () => {
+  const { handleHoverEnter, handleHoverLeave } = useCursor();
+
   const navigate = useNavigate();
 
   return (
     <nav className="nav">
       <ul className="nav__list">
-        <li className="nav__item">
-          <motion.a
-            className="nav__link"
-            onClick={() => navigate("/")}
-            initial={navLinkInitial}
-          >
-            Home
-          </motion.a>
-        </li>
-        <li className="nav__item">
-          <motion.a
-            className="nav__link"
-            onClick={() => navigate("/about")}
-            initial={navLinkInitial}
-          >
-            About
-          </motion.a>
-        </li>
-        <li className="nav__item">
-          <motion.a
-            className="nav__link"
-            onClick={() => navigate("/projects")}
-            initial={navLinkInitial}
-          >
-            Projects
-          </motion.a>
-        </li>
-        <li className="nav__item">
-          <motion.a
-            className="nav__link"
-            onClick={() => navigate("/contact")}
-            initial={navLinkInitial}
-          >
-            Contact
-          </motion.a>
-        </li>
+        {pages.map(({ title, url }) => (
+          <li key={url} className="nav__item">
+            <motion.a
+              className="nav__link"
+              onClick={() => navigate(url)}
+              initial={navLinkInitial}
+              onHoverStart={handleHoverEnter}
+              onHoverEnd={handleHoverLeave}
+            >
+              {title}
+            </motion.a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
