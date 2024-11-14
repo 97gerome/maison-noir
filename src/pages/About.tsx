9610c4ce6bf2d060818a92ddc47e56620778bf7c
Gerome Mandapat <geromemandapat@Geromes-MacBook-Pro-2.local>
2024-11-14 20:03:52 +0800

    Added the Our Process and Work With Us sections and the Footer.

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,8 +5,14 @@ import TeamCard, { TeamCardProps } from "../components/TeamCard";
 import graceCarter from "../assets/images/grace-carter.png";
 import isabellaPreston from "../assets/images/isabella-preston.png";
 import owenMatthews from "../assets/images/owen-matthews.png";
-import "react-multi-carousel/lib/styles.css";
 import Carousel from "../components/Carousel";
+import { CollapsibleItemProps } from "../components/CollapsibleItem";
+import CollapsibleList from "../components/CollapsibleList";
+import Button from "../components/Button";
+import logo from "../assets/logo.svg";
+import instagramLogo from "../assets/instagram-logo.svg";
+import facebookLogo from "../assets/facebook-logo.svg";
+import linkedinLogo from "../assets/linkedin-logo.svg";
 
 const teamMembers: Array<TeamCardProps> = [
   {
@@ -23,6 +29,112 @@ const teamMembers: Array<TeamCardProps> = [
     img: owenMatthews,
     name: "Owen Matthews",
     role: "Project Manager",
+  },
+];
+
+const ourProcess: Array<CollapsibleItemProps> = [
+  {
+    title: "Initial Consultation",
+    body: (
+      <>
+        The work process for Maison Noir begins with an initial consultation,
+        where the client and the design team meet to discuss the project&apos;s
+        overall vision. This involves understanding the client&apos;s
+        preferences, lifestyle, and goals for the space, whether it&apos;s a
+        residential or commercial property. Following this, Maison Noir conducts
+        a thorough site visit to evaluate the existing space. They assess the
+        structure, lighting, and current layout, determining how best to enhance
+        the environment. During this stage, both the client and designer define
+        the scope of the project, including the number of rooms, any required
+        structural changes, and budget constraints. This first step is all about
+        laying the groundwork for a successful design.
+      </>
+    ),
+  },
+  {
+    title: "design concept development",
+    body: (
+      <>
+        Next comes the design concept development stage. Here, the team creates
+        mood boards that capture the overall style and aesthetic direction for
+        the project. These boards are filled with inspiration images, color
+        palettes, textures, and ideas for furniture styles and lighting. To give
+        the client a tangible sense of the design, preliminary sketches and
+        basic layouts are also created. These plans help visualize the
+        arrangement of furniture, space utilization, and general flow. Client
+        feedback is crucial during this phase; the initial concept is refined
+        according to the client&apos;s preferences, ensuring the project aligns
+        with their expectations.
+      </>
+    ),
+  },
+  {
+    title: "detailed design & planning",
+    body: (
+      <>
+        The third step involves detailed design and planning. Once the initial
+        concept is approved, Maison Noir develops comprehensive 3D renderings
+        and floor plans. These visual representations allow the client to see
+        how the final space will look and feel, with precise detail on textures,
+        colors, and furniture placement. The design team also presents carefully
+        curated selections for materials and finishes, including fabrics,
+        flooring, lighting, and hardware. Along with these visual and material
+        selections, a budget proposal is provided, detailing all costs for
+        materials, labor, and design fees. A clear project timeline is then
+        outlined, specifying key milestones and deadlines to keep the project on
+        track.
+      </>
+    ),
+  },
+  {
+    title: "procurement & project management",
+    body: (
+      <>
+        With the design finalized, the process moves into procurement and
+        project management. This stage involves sourcing all the necessary
+        furniture, materials, and décor, often from high-quality suppliers or
+        custom-made orders. The project manager works closely with contractors,
+        ensuring that the team follows the design specifications accurately.
+        They also coordinate with electricians, plumbers, and other specialists
+        for any technical work that might be required. Regular site visits
+        ensure that the project is progressing smoothly, and the client is kept
+        informed with frequent updates on milestones, addressing any changes or
+        challenges as they arise.
+      </>
+    ),
+  },
+  {
+    title: "Installation & styling",
+    body: (
+      <>
+        Once the construction or renovation phase is complete, the focus shifts
+        to installation and styling. The design team oversees the installation
+        of furniture, lighting, and other decorative elements. They ensure each
+        piece is placed optimally for both functionality and aesthetics. The
+        final styling stage involves adding personal touches—artwork,
+        accessories, textiles, and plants—that breathe life into the space.
+        After everything is set, Maison Noir conducts a final walkthrough with
+        the client. This is the moment when the client experiences the finished
+        design in person, offering feedback and discussing any last-minute
+        adjustments.
+      </>
+    ),
+  },
+  {
+    title: "post-project follow-up",
+    body: (
+      <>
+        Finally, post-project follow-up ensures that the client is fully
+        satisfied with the completed design. Maison Noir handles any necessary
+        touch-ups or adjustments based on the client&apos;s input after the
+        final walkthrough. The team also offers aftercare services, providing
+        maintenance tips for materials and ongoing support if the client needs
+        additional help down the road. Feedback is collected to continuously
+        improve the business, ensuring that future projects benefit from each
+        client&apos;s experience. This step ensures lasting client relationships
+        and long-term satisfaction with the services provided.
+      </>
+    ),
   },
 ];
 
@@ -98,7 +210,7 @@ const About: FC = () => {
       </section>
       <section className="about-team">
         <div className="about-team__container--heading">
-          <h2 className="about-team__heading">MEET OUR TEAM</h2>
+          <h2 className="about-team__heading">Meet Our Team</h2>
         </div>
         <Carousel
           className="about-team__carousel"
@@ -106,6 +218,57 @@ const About: FC = () => {
           renderComponent={(data) => <TeamCard {...data} />}
         ></Carousel>
       </section>
+      <section className="about-process">
+        <h5 className="about-process__heading">Our Process</h5>
+        <div className="about-process__container--process">
+          <CollapsibleList items={ourProcess} />
+        </div>
+        <p className="about-process__desc">
+          Our personalized design process provides a seamless experience,
+          ensuring each client&apos;s vision is central to every decision. With
+          meticulous planning and quality execution, we transform spaces into
+          stunning reflections of individual style while delivering projects on
+          time and within budget. Clients can confidently watch their dream
+          interiors come to life, supported every step of the way.
+        </p>
+      </section>
+      <section className="about-work-w-us">
+        <h2 className="about-work-w-us__heading">Work with us</h2>
+        <div className="about-work-w-us__content">
+          <p className="about-work-w-us__text">
+            Ready to create something extraordinary together?
+            <br />
+            Reach out today, and let&apos;s turn your dream space into a
+            stunning reality.
+          </p>
+          <div className="about-work-w-us__container--button">
+            <Button label="Get in touch" />
+          </div>
+        </div>
+      </section>
+      <footer className="about-footer">
+        <img src={logo} alt="Maison Noir" className="about-footer__logo" />
+        <p className="about-footer__copyright">
+          Copyright 2024 © MAISON NOIR Interior Design
+        </p>
+        <div className="about-footer__container--socials">
+          <img
+            src={instagramLogo}
+            alt="Instagram"
+            className="about-footer__social"
+          />
+          <img
+            src={facebookLogo}
+            alt="Facebook"
+            className="about-footer__social"
+          />
+          <img
+            src={linkedinLogo}
+            alt="LinkedIn"
+            className="about-footer__social"
+          />
+        </div>
+      </footer>
     </main>
   );
 };
